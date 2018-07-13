da5cd1a082c60690993b45ecf918cb97d72bbc9f
Edwin <edhernansol@gmail.com>
2018-07-13 16:24:20 -0600

    Cambios plantilla correo

---
+++ WebSite/Utiles/GeneracionCodigo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Utiles
+{
+	public class GeneracionCodigo
+	{
+		public string GenerarCodigoUnico()
+		{
+			Guid guid = Guid.NewGuid();
+			string codigoUnico = Convert.ToBase64String(guid.ToByteArray());
+			codigoUnico = codigoUnico.Replace("=", "");
+			codigoUnico = codigoUnico.Replace("+", "");
+
+			return codigoUnico;
+		}
+	}
+}
